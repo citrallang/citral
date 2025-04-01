38cf36ba62e4bd67f32474c832863f3ab4830b57
cg223 <cg223celeste@gmail.com>
2025-04-01 05:36:32 -0400

    start grammar

--- citral/grammar.h
+++ citral/grammar.h
@@ -1,8 +1,10 @@
 #pragma once
 //grammar
 /*
-
-
+program = block
+block = {statements
+statements = statement [}statements]
+statement = exprStmt | block | whileStmt | forStmt | ifStmt | switchStmt
 
 
 
