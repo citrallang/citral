68a4692246feb0dbf2a97124ae4caad01ff5dfcc
cg223 <cg223celeste@gmail.com>
2025-04-28 19:05:13 -0400

    adhere to naming conventions here

--- citral/hashmap.h
+++ citral/hashmap.h
@@ -6,20 +6,24 @@
 
 typedef union hash_keyval {
 	void* asPtr;
-	uint64_t asui64;
-	double asdouble;
-	int64_t asi64;
+	uint64_t asUI64;
+	double asDouble;
+	int64_t asI64;
 } hash_keyval;
 
 typedef struct hash_node {
 	hash_keyval key;
 	hash_keyval val;
-	unsigned int keysize; //these can be used for additional info if the type bits are set to the non pointer values
+	unsigned int keySize; //these can be used for additional info if the type bits are set to the non pointer values
 	unsigned int valsize;
-	unsigned int key_type : 2;
-	unsigned int val_type : 2;
+	unsigned int keyType : 2;
+	unsigned int valType : 2;
 	unsigned int isGrave : 1;
 	unsigned int isEmpty : 1;
 } hash_node;
 
 
+typedef struct hash_table {
+	hash_node* nodes;
+	unsigned int numNodes;
+};
