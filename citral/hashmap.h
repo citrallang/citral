14b8312a06d4c0d0eb7edd0c24bad4e669a8a27f
cg223 <cg223celeste@gmail.com>
2025-04-28 19:11:27 -0400

    evil function pointers. be scared

--- citral/hashmap.h
+++ citral/hashmap.h
@@ -4,26 +4,32 @@
 //ALL METHODS HERE SHOULD BE STATIC
 //traditional quadratically probed hash table.
 
-typedef union hash_keyval {
+typedef union HashKeyVal {
 	void* asPtr;
 	uint64_t asUI64;
 	double asDouble;
 	int64_t asI64;
-} hash_keyval;
+} HashKeyVal;
 
-typedef struct hash_node {
-	hash_keyval key;
-	hash_keyval val;
+typedef struct HashNode {
+	HashKeyVal key;
+	HashKeyVal val;
 	unsigned int keySize; //these can be used for additional info if the type bits are set to the non pointer values
 	unsigned int valsize;
 	unsigned int keyType : 2;
 	unsigned int valType : 2;
 	unsigned int isGrave : 1;
 	unsigned int isEmpty : 1;
-} hash_node;
+} HashNode;
 
+typedef union HashFunc {
+	long (*asPtrFunc)(char*, unsigned int);
+	long (*asLongFunc)(long);
+} HashFunc;
 
-typedef struct hash_table {
-	hash_node* nodes;
+typedef struct HashTable {
+	HashNode* nodes;
 	unsigned int numNodes;
-};
+	unsigned int maxNodes;
+	HashFunc hasher; //idk if i want to do this but hey
+} HashTable;
