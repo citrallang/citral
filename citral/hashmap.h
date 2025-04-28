8b3cf18a6da6a31de7436fe2e4dadf360a35dcd0
cg223 <cg223celeste@gmail.com>
2025-04-28 18:59:35 -0400

    im not doing allat

--- citral/hashmap.h
+++ citral/hashmap.h
@@ -14,15 +14,8 @@ typedef union hash_keyval {
 typedef struct hash_node {
 	hash_keyval key;
 	hash_keyval val;
-	int keysize;
-	int valsize;
+	unsigned int keysize;
+	unsigned int valsize;
 } hash_node;
 
-typedef struct hash_node_sizeless {
-	hash_keyval key;
-	hash_keyval val;
-};
 
-//SAFE: evaluates a once
-#define hash_node_as_sizeless(a) (*((hash_node_sizeless*)(&(a))))
-#define hash_node_as_sized(a) (*((hash_node*)(&(a))))
