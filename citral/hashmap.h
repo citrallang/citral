f2633f4f45410867bde5be6b2443311bec6272d9
cg223 <cg223celeste@gmail.com>
2025-04-28 19:02:35 -0400

    add some stuff i might not use. i havent made a good hashtable in a looong time

--- citral/hashmap.h
+++ citral/hashmap.h
@@ -14,8 +14,12 @@ typedef union hash_keyval {
 typedef struct hash_node {
 	hash_keyval key;
 	hash_keyval val;
-	unsigned int keysize;
+	unsigned int keysize; //these can be used for additional info if the type bits are set to the non pointer values
 	unsigned int valsize;
+	unsigned int key_type : 2;
+	unsigned int val_type : 2;
+	unsigned int isGrave : 1;
+	unsigned int isEmpty : 1;
 } hash_node;
 
 
