e44f94cde8089d0ab5179418f2bd3a7ecc11bf92
cg223 <cg223celeste@gmail.com>
2025-05-12 21:20:55 -0400

    lets not violate C standard for no reason

--- citral/rules.h
+++ citral/rules.h
@@ -7,7 +7,7 @@ Function names in this project are snake_case.
 Types are PascalCase.
 Variables are camelCase.
 Constants (regardless of scoping) are ALL_CAPS snake case.
-Internal components/functions are prefixed with _ or "internal".
+Internal components/functions are prefixed with __ or "internal".
 
 
 Variables or functions associated with a section of the compiler always start with that sections name.
